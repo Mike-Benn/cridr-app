41ceba0b039325d87c9882176f44e5583a391562
Mike-Benn <mbenn49@wgu.edu>
2024-09-05 19:50:20 -0400

    Added skeleton for top nav bar

---
+++ src/components/header/TopNav.jsx
@@ -0,0 +1,21 @@
+
+
+
+function TopNav() {
+
+    return (
+        <header>
+            <h2>{document.title.split(' ')[0]}</h2>
+            <p>profpicture</p>
+        </header>
+    )
+}
+
+
+
+
+
+
+
+
+export default TopNav
