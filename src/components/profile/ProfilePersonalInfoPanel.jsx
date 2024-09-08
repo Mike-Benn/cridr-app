fe66bf73f6e09e86bd054b2972373c3d760e15b1
Mike-Benn <mbenn49@wgu.edu>
2024-09-08 07:30:00 -0400

    Add personal information panel to profile

---
+++ src/components/profile/ProfilePersonalInfoPanel.jsx
@@ -0,0 +1,14 @@
+
+
+function ProfilePersonalInfoPanel() {
+    return (
+        <form action="">
+            <fieldset>
+                <legend>Account Information</legend>
+                <input type="text" placeholder="Full Name"></input>
+            </fieldset>
+        </form>
+    )
+}
+
+export default ProfilePersonalInfoPanel
