96e9c4e1fed0c74db6e5511bc101d68820e1b540
Mike-Benn <mbenn49@wgu.edu>
2024-09-09 06:08:50 -0400

    Add manage cards panel for credit cards

---
+++ src/components/profile/ManageCardsPanel.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useEffect , useState } from "react";
+import { v4 as uuidv4 } from "../../../node_modules/uuid"
+import CardListItem from "./CardListItem";
+
+
+function ManageCardsPanel() {
+    const [cardList , setCardList] = useState([]);
+    const cardListItems = cardList.map(item => 
+        <CardListItem key={uuidv4()} data={item} />
+    )
+
+    useEffect(() => {
+        const fetchCards = async () => {
+            try {
+                const response = await axios.get('https://api.mike-benn.com/profile/credit-cards/get-cards')
+                if (response.status === 200) {
+                    setCardList(response.data.split(", "));
+                } else {
+                    console.error('Failed to fetch cards:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error while fetching cards:', error);
+            }
+        };
+
+        fetchCards();
+    }, []);
+
+    return (
+        <ul>{cardListItems}</ul>
+    )
+}
+
+export default ManageCardsPanel
