68bdf167907c1efe852685830594a2e44d9dc837
Mike-Benn <mbenn49@wgu.edu>
2024-09-11 07:15:00 -0400

    Add pesonal info section for profile settings Add axios request to get name

--- src/components/profile/ProfilePersonalInfoPanel.jsx
+++ src/components/profile/ProfilePersonalInfoPanel.jsx
@@ -1,11 +1,36 @@
-
+import { useState, useEffect } from "react"
+import axios from "axios";
 
 function ProfilePersonalInfoPanel() {
+    const [fullName , setFullName] = useState("");
+
+    useEffect(() => {
+        const fetchName = async () => {
+            try {
+                const response = await axios.get('https://api.mike-benn.com/profile/personal/get-name');
+                if (response.status === 200) {
+                    setFullName(response.data.data);
+                } else {
+                    console.error("Failed to fetch full name");
+                }
+            } catch (error) {
+                console.error("Failed to fetch full name", error);
+            }
+        };
+        fetchName();
+    }, []);
+
+    const handleNameChange = (e) => {
+        setFullName(e.target.value);
+    };
+
+
     return (
-        <form action="">
+        <form action="" >
             <fieldset>
                 <legend>Account Information</legend>
-                <input type="text" placeholder="Full Name"></input>
+                <input type="text" placeholder="Full Name" value={fullName} onChange={handleNameChange}></input>
+                <button type="submit"></button>
             </fieldset>
         </form>
     )
