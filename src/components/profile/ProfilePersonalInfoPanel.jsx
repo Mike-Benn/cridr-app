138a2e0ad76950e09a85b3ab1d82cc6e8b565ebb
Mike-Benn <mbenn49@wgu.edu>
2024-09-23 07:24:10 -0400

    Add post request to save personal info changes

--- src/components/profile/ProfilePersonalInfoPanel.jsx
+++ src/components/profile/ProfilePersonalInfoPanel.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react"
 import axios from "axios";
+import SubmitFormButton from "../general/buttons/SubmitFormButton";
+import TextField from "../general/form-fields/TextField";
+
 
 function ProfilePersonalInfoPanel() {
     const [fullName , setFullName] = useState("");
+    
 
     useEffect(() => {
         const fetchName = async () => {
@@ -24,13 +28,36 @@ function ProfilePersonalInfoPanel() {
         setFullName(e.target.value);
     };
 
+    const handleSubmitForm = async (e) => {
+        e.preventDefault();
+
+        const newUpdate = {
+            fullName,
+        };
+
+        try {
+            const response = await axios.post('https://api.mike-benn.com/profile/personal/submit-info-change', newUpdate, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.status === 200) {
+                console.log('Profile changes saved:', response.data);
+            } else {
+                console.error('Failed to save changes:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error while saving changes:', error);
+        }
+    };
 
     return (
-        <form action="" >
+        <form action="" onSubmit={handleSubmitForm}>
             <fieldset>
                 <legend>Account Information</legend>
-                <input type="text" placeholder="Full Name" value={fullName} onChange={handleNameChange}></input>
-                <button type="submit"></button>
+                <TextField placeholder="Full Name" value={fullName} onChange={handleNameChange} />
+                <SubmitFormButton buttonText="Save Changes" />
             </fieldset>
         </form>
     )
