b9e8b6e02b90213159dcaba74c7f8d63262bf881
Mike-Benn <mbenn49@wgu.edu>
2024-11-18 07:16:30 -0500

    add skeleton of fetch coupons effect

--- src/components/card-coupons/CreditCardCouponPanel.jsx
+++ src/components/card-coupons/CreditCardCouponPanel.jsx
@@ -4,14 +4,37 @@ import { COUPON_TYPES_FILTER, SORT_OPTIONS } from "../../utils/coupons/options";
 import { useNavigate } from "react-router-dom";
 import glass from "../../images/magnifying.png"
 import CouponList from "./CouponList";
-import { useEffect } from "react";
+
+import { useEffect} from "react";
+import axios from "axios";
+
 
 function CreditCardCouponPanel() {
-
+    /*const [couponList, setCouponList] = useState([]);
+    const couponListItems = couponList.map(item => 
+        <CouponListItem key={uuidv4()} data={item} />
+    )*/
+    const apiUrl = import.meta.env.VITE_API_URL;
     useEffect(() => {
         document.title = "Coupons | Cridr";
-    }, []);
+        const fetchCoupons = async () => {
+            try {
+                const response = await axios.get(`${apiUrl}/coupon/all`)
+                if (response.status === 200) {
+                    console.log(response.data)
+                } else {
+                    console.error('Failed to fetch coupons:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error while fetching coupons:', error);
+            }
+        };
+
+        fetchCoupons();
+    }, [apiUrl]);
+
     const navigate = useNavigate();
+
     const handleAddCoupon = () => {
         navigate('/coupon/new')
     }
