8bae3364cb8679b3a88e3c461e162ac9e8760d36
Mike-Benn <mbenn49@wgu.edu>
2024-11-25 07:50:54 -0500

    Implement time left on coupon util function

--- src/components/card-coupons/CouponListItem.jsx
+++ src/components/card-coupons/CouponListItem.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types'
+import { getReadableDate } from '../../utils/general/utils';
 
 function CouponListItem({ data }) {
+    let expirationDate = getReadableDate(data.expiration_date);
+
     return (
         <li>
-            <p>{data.card_name} x {data.business_name}</p>
+            <p>{data.card_name} - {data.business_name} - {expirationDate}</p>
         </li>
     )
 }
