e3ba450bea4e6dc0e4de070c0a3c7691b63967a4
Mike-Benn <mbenn49@wgu.edu>
2024-12-16 07:46:06 -0500

    Add fetching for business names by feature type

--- src/components/incentives/AddIncentivesPanel.jsx
+++ src/components/incentives/AddIncentivesPanel.jsx
@@ -1,14 +1,36 @@
-import { useState } from "react"
+import { useState , useEffect} from "react"
 import TextField from "../general/form-fields/TextField"
+import SelectField from "../general/form-fields/SelectField";
 import NumberField from "../general/form-fields/NumberField";
 import DateField from "../general/form-fields/DateField";
+import axios from 'axios'
 
 function AddIncentivePanel() {
-
+    const apiUrl = import.meta.env.VITE_API_URL;
     const [businessName , setBusinessName] = useState("");
     const [incentiveName , setIncentiveName] = useState("");
     const [amount , setAmount] = useState("");
     const [transactionDate , setTransactionDate] = useState(null);
+    const [businessOptions , setBusinessOptions] = useState([]);
+
+    console.log(businessOptions);
+
+    useEffect(() => {
+        document.title = "Add Incentive | Cridr";
+        const fetchBusinesses = async () => {
+            try {
+                const response = await axios.get(`${apiUrl}/utils/get-businesses/Incentives`);
+                if (response.data) {
+                    setBusinessOptions(response.data.data);
+                }
+            } catch (error) {
+                console.error(error , "Error fetching business names");
+            }
+        }
+
+        fetchBusinesses();
+
+    }, [apiUrl])
 
     const handleBusinessNameChange = (e) => {
         setBusinessName(e.target.value);
@@ -30,7 +52,7 @@ function AddIncentivePanel() {
         <form action="">
             <fieldset>
                 <legend>Add Incentive Transaction</legend>
-                <TextField fieldName="Business Name" onChange={handleBusinessNameChange} value={businessName} />
+                
                 <TextField fieldName="Incentive Name" onChange={handleIncentiveNameChange} value={incentiveName} />
                 <NumberField fieldName="Amount" onChange={handleAmountChange} value={amount} />
                 <DateField fieldName="Transaction Date" onChange={handleTransactionDateChange} value={transactionDate} />
