1bf7625633e632f4e9b6161998f4990bcfe94f24
Mike-Benn <mbenn49@wgu.edu>
2024-12-30 07:48:34 -0500

    Rework panel to add credit card coupons to db

--- src/components/card-coupons/AddCouponPanel.jsx
+++ src/components/card-coupons/AddCouponPanel.jsx
@@ -1,105 +1,139 @@
-import SelectField from '../general/form-fields/SelectField'
-import TextField from '../general/form-fields/TextField'
 import NumberField from '../general/form-fields/NumberField'
 import DateField from "../general/form-fields/DateField"
 import SubmitFormButton from "../general/buttons/SubmitFormButton"
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { COUPON_TYPES , CASHBACK_TYPE } from '../../utils/coupons/couponOptions'
-import { CARD_OPTIONS } from '../../utils/cards/cardOptions'
 import { useNavigate } from 'react-router-dom'
+import { addDefaultOptionToSelect } from '../../utils/general/utils'
+import VariableSelectField from '../general/form-fields/VariableSelectField'
 
 function AddCouponPanel() {
 
-    const [card , setCard] = useState("default");
-    const [couponType , setCouponType] = useState("default");
-    const [cashbackType , setCashbackType] = useState("default");
-    const [businessName , setBusinessName] = useState("");
-    const [amount , setAmount] = useState("");
-    const [cashback_limit , setLimit] = useState("");
+    const [userId , setUserId] = useState(1);
+    const [cardId , setCardId] = useState("-1");
+    const [cashbackTypeId , setCashbackTypeId] = useState("-1");
+    const [cardOptions , setCardOptions] = useState([]);
+    const [businessOptions , setBusinessOptions] = useState([]);
+    const [cashbackTypeOptions , setCashbackTypeOptions] = useState([]);
+    const [businessId , setBusinessId] = useState("-1");
+    const [cashbackRate , setCashbackRate] = useState("");
+    const [cashbackLimit , setCashbackLimit] = useState("");
     const [expirationDate , setExpirationDate] = useState("");
     const navigate = useNavigate();
     const apiUrl = import.meta.env.VITE_API_URL;
+
     useEffect(() => {
         document.title = "Add Coupon | Cridr";
-    }, []);
+        const fetchCreditCards = async () => {
+            try {
+                const response = await axios.get(`${apiUrl}/utils/get-credit-cards-by-id/${userId}`) 
+                if (response.data) {
+                    let optionList = [...response.data.data];
+                    addDefaultOptionToSelect(optionList , "credit_card_name" , "credit_card_id" , "Select Credit Card");
+                    setCardOptions(optionList);
+                }
+            } catch (error) {
+                console.error(error , "Failed to fetch cards.");
+            }
+        }
+        const fetchBusinessesByFeature = async () => {
+            try {
+                const response = await axios.get(`${apiUrl}/businesses/feature/Coupons`);
+                if (response.data) {
+                    let optionList = [...response.data.data];
+                    addDefaultOptionToSelect(optionList , "business_name" , "business_id" , "Select Business/Service Provider");
+                    setBusinessOptions(optionList);
+                }
+            } catch (error) {
+                console.error(error , "Failed to fetch businesses.")
+            }
+        }
+        const fetchCashbackTypes = async () => {
+        
+            try {
+                const response = await axios.get(`${apiUrl}/cashback/type/all`);
+                
+                if (response.data) {
+                    let optionList = [...response.data.data];
+                    addDefaultOptionToSelect(optionList , "coupon_cashback_type_desc" , "coupon_cashback_type_id" , "Select Cashback Type");
+                    setCashbackTypeOptions(optionList);
+                }
+            } catch (error) {
+                console.error(error , "Failed to fetch cashback types.");
+            }
+        }
+        fetchCreditCards();
+        fetchBusinessesByFeature();
+        fetchCashbackTypes();
 
-    const handleCardChange = (e) => {
-        setCard(e.target.value);
+    }, [apiUrl , userId]);
+
+    const handleCardIdChange = (e) => {
+        setCardId(e.target.value);
     }
 
-    const handleCouponTypeChange = (e) => {
-        setCouponType(e.target.value);
-    }
-
-    const handleCashbackTypeChange = (e) => {
-        setCashbackType(e.target.value);
+    const handleCashbackTypeIdChange = (e) => {
+        setCashbackTypeId(e.target.value);
     }
     
-    const handleBusinessNameChange = (e) => {
-        setBusinessName(e.target.value);
+    const handleBusinessIdChange = (e) => {
+        setBusinessId(e.target.value);
     }
 
-    const handleAmountChange = (e) => {
-        setAmount(e.target.value);
+    const handleCashbackRateChange = (e) => {
+        setCashbackRate(e.target.value);
     }
 
-    const handleLimitChange = (e) => {
-        setLimit(e.target.value);
+    const handleCashbackLimitChange = (e) => {
+        setCashbackLimit(e.target.value);
     }
 
     const handleExpirationDateChange = (e) => {
         setExpirationDate(e.target.value);
     }
-
-    
-const handleSubmitForm = async (e) => {
-    e.preventDefault();
-
-    const newCoupon = {
-        card,
-        couponType,
-        cashbackType,
-        businessName,
-        amount: Number(amount),
-        cashback_limit: Number(cashback_limit),
-        expirationDate,
-        username: "Mike",
-    };
-    try {
-        const response = await axios.post(`${apiUrl}/coupon/submit-coupon`, newCoupon);
-
-        if (response.status === 200) {
-            console.log('Coupon added successfully:', response.data);
-            clearForm();
-            navigate('/coupon');
-        } 
-    } catch (error) {
-        console.error('Error while adding coupon:', error);
-    }
-};
-    
-    const clearForm = () => {
-        setCard("default");
-        setCouponType("default");
-        setCashbackType("default");
-        setBusinessName("");
-        setAmount("");
-        setLimit("");
+    const handleClearForm = () => {
+        setCardId("-1");
+        setCashbackTypeId("-1");
+        setBusinessId("-1");
+        setCashbackRate("");
+        setCashbackLimit("");
         setExpirationDate("");
     }
+    const handleCouponFormSubmit = async (e) => {
+        e.preventDefault();
 
+        const newCoupon = {
+            user_id: userId,
+            credit_card_id: cardId,
+            business_id: businessId,
+            coupon_cashback_type_id: cashbackTypeId,
+            coupon_cashback_rate: cashbackRate,
+            coupon_cashback_limit: cashbackLimit,
+            coupon_expiration_date: expirationDate,
+        };
+
+        try {
+            const response = await axios.post(`${apiUrl}/coupon/submit-coupon` , newCoupon);
+            if (response.status === 200) {
+                handleClearForm();
+                navigate("/coupon");
+            }
+
+        } catch (error) {
+            console.error(error , "Failed to post coupon.")
+        }
+    }
+    
     return (
-        <form action='' onSubmit={handleSubmitForm}>
+        <form action='' onSubmit={handleCouponFormSubmit}>
             <fieldset>
                 <legend>Coupon Info</legend>
-                <SelectField options={CARD_OPTIONS} onChange={handleCardChange} value={card} name="cardName"/>
-                <SelectField options={COUPON_TYPES} onChange={handleCouponTypeChange} value={couponType} name="couponType"/>
-                <SelectField options={CASHBACK_TYPE} onChange={handleCashbackTypeChange} value={cashbackType} name="cashbackType"/>
-                <TextField fieldName="Establishment/Service Name" onChange={handleBusinessNameChange} value={businessName} name="businessName"/>
-                <NumberField fieldName="Cashback Amount" placeHolder="% or raw amount, must be number" onChange={handleAmountChange} value={amount} name="amount"/>
-                <NumberField fieldName="Cashback Limit" placeHolder="Must be number, 0 for no limit" onChange={handleLimitChange} value={cashback_limit} name="limit"/>
-                <DateField fieldName="Expiration Date" onChange={handleExpirationDateChange} value={expirationDate} name="expirationDate"/>
+                <VariableSelectField fieldId="card-coupons-card-select" labelText="Select Credit Card" optionList={cardOptions} onChange={handleCardIdChange} value={cardId} optionIdAccessor="credit_card_id" optionTextAccessor="credit_card_name" />
+                <VariableSelectField fieldId="card-coupons-cashback-select" labelText="Select Cashback Type"  optionList={cashbackTypeOptions} onChange={handleCashbackTypeIdChange} value={cashbackTypeId} optionIdAccessor="coupon_cashback_type_id" optionTextAccessor="coupon_cashback_type_desc" />
+                <VariableSelectField fieldId="card-coupons-business-select" labelText="Business/Service Provider" optionList={businessOptions} onChange={handleBusinessIdChange} value={businessId} optionIdAccessor="business_id" optionTextAccessor="business_name" />
+                <NumberField fieldName="Cashback Rate" placeHolder="% or raw amount, must be number" onChange={handleCashbackRateChange} value={cashbackRate} />
+                <NumberField fieldName="Cashback Limit" placeHolder="Must be number, 0 for no limit" onChange={handleCashbackLimitChange} value={cashbackLimit} />
+                <DateField fieldName="Expiration Date" onChange={handleExpirationDateChange} value={expirationDate} />
                 <SubmitFormButton buttonText='Submit Coupon'/>
             </fieldset>
         </form>
