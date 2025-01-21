c858b4512b183a4446f54590c6ed8396ae8219d2
Mike-Benn <mbenn49@wgu.edu>
2025-01-21 05:44:24 -0500

    Add logout button

--- src/components/header/TopNav.jsx
+++ src/components/header/TopNav.jsx
@@ -1,17 +1,28 @@
 import { useLocation } from "react-router-dom"
 import { getPathName } from "../../utils/general/utils"
+import GeneralButton from "../general/buttons/GeneralButton"
+import { useContext } from "react";
+import AuthContext from "../../auth/AuthContext";
+
 
 function TopNav() {
-
+    const { logout } = useContext(AuthContext);
     let path = useLocation();
     path = getPathName(path.pathname);
     
-    
+    const handleUserLogOut = async () => {
+        try {
+            await logout();
+        } catch (error) {
+            console.error(error , "There was an issue communicating with the server.");
+        }
+    }
     // Home, prof picture
     return (
         <header>
             <h2>{path}</h2>
             <p>Top Navbar</p>
+            <GeneralButton buttonType="button" buttonText="Log Out" onClick={handleUserLogOut} />
         </header>
     )
 }
