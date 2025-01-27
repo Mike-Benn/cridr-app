e2acbc93232d35f2206d4d21189bd34e920c9e06
Mike-Benn <mbenn49@wgu.edu>
2025-01-27 02:00:39 -0500

    remove consolelog

--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
     const { isAuthenticated } = useContext(AuthContext);
     const navigate = useNavigate();
-    console.log("test")
     useEffect(() => {
         if (!isAuthenticated) {
             navigate("/log-in");
