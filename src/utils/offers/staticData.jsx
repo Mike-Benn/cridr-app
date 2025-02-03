f1b8fb6b7c0c68204351f4c0e4c3b680e70b71be
Mike-Benn <mbenn49@wgu.edu>
2025-02-03 07:00:09 -0500

    Static cashback type options for select field

---
+++ src/utils/offers/staticData.jsx
@@ -0,0 +1,16 @@
+
+const cashbackTypeOptions = [
+    {
+        cashback_type_id: "default",
+        cashback_type_text: "Select Cashback Type",
+    },
+    {
+        cashback_type_id: "percent",
+        cashback_type_text: "Percent Cashback",
+    },
+    {
+        cashback_type_id: "flat",
+        cashback_type_text: "Flat Cashback",
+    },
+]
+export { cashbackTypeOptions };
