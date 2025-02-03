0f82c3612d219cd2773ac33e88d3499b106b01be
Mike-Benn <mbenn49@wgu.edu>
2025-02-03 06:56:50 -0500

    Create list item for credit card offer records

---
+++ src/utils/offers/utils.jsx
@@ -0,0 +1,16 @@
+import DeleteListItemButton from "../../components/general/buttons/DeleteListItemButton";
+import PropTypes from "prop-types";
+
+
+function OfferListItem({ offer, onDelete }) {
+    return <li>{offer.credit_card_name} - {offer.available_offer_participating_business} - {offer.cashback_rate}% - Expires: {new Date(offer.available_offer_expiration_date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })} <DeleteListItemButton id={offer.available_offer_id} onClick={onDelete} /> </li>
+
+}
+
+OfferListItem.propTypes = {
+    offer: PropTypes.object,
+    onDelete: PropTypes.func,
+}
+export {
+    OfferListItem,
+}
