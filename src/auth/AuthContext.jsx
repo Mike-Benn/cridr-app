bb3df5a62723777ec54ba0982e495a0282020d33
Mike-Benn <mbenn49@wgu.edu>
2025-02-24 07:40:21 -0500

    Add isLoading to context

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -3,6 +3,7 @@ import { createContext } from "react"
 
 const AuthContext = createContext({
     isAuthenticated: false,
+    isLoading: false,
     login: () => {},
     logout: () => {},
     checkAuthenticationStatus: () => {},
