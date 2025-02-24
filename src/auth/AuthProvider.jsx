23239508163e5b70dee1bcf6835d5bd1a519be94
Mike-Benn <mbenn49@wgu.edu>
2025-02-24 07:39:09 -0500

    Add isLoading check to auth context

--- src/auth/AuthProvider.jsx
+++ src/auth/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import apiClient from "../api/apiClient";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "./AuthContext";
@@ -7,37 +7,47 @@ import AuthContext from "./AuthContext";
 const AuthProvider = ({ children }) => {
     const navigate = useNavigate();
     const [isAuthenticated , setIsAuthenticated] = useState(false)
-
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        const token = localStorage.getItem("accessToken");
+        if (token) {
+            setIsAuthenticated(true);
+        }
+        setIsLoading(false);
+    }, [])
+    
     const login = async (credentials) => {
         try {
-            const response = await apiClient.post("/auth/log-in" , credentials);
-            if (response.data.isAuthenticated) {
+            const response = await apiClient.post("/auth/login", credentials);
+            if (response.data?.data) {
+                localStorage.setItem("accessToken", response.data.data);
                 setIsAuthenticated(true);
                 navigate("/")
-
+                
             }
+
         } catch (error) {
-            console.error(error, "Unnable to log you in.");
+            console.error("Unable to login", error);
+            setIsAuthenticated(false);
         }
     }
-    
+
     const logout = async () => {
         try {
-            const response = await apiClient.post("/log-out");
-            if (response.status === 200) {
-                setIsAuthenticated(false);
-                navigate("/log-in");
-            }
+            await apiClient.post("/auth/logout");
+            localStorage.removeItem("accessToken");
+            setIsAuthenticated(false);
+            navigate("/log-in")
         } catch (error) {
-            console.error(error , "There was an issue communicating with the server.");
+            console.error(error , "There was an error logging you out.");
         }
     }
 
     
 
     return (
-        <AuthContext.Provider value={ { login , logout , isAuthenticated }}>
-            {children}
+        <AuthContext.Provider value={ { login , logout , isAuthenticated, isLoading }}>
+            {isLoading ? null : children}
         </AuthContext.Provider>
     )
 
