dd84ccb1ec68282c906a20aa08d7fe4d4312c64b
Mike-Benn <mbenn49@wgu.edu>
2025-03-10 07:34:38 -0400

    Add extra key for net gas expenditure Rename previous gas expenditure to gross

--- src/utils/fuel/utils.jsx
+++ src/utils/fuel/utils.jsx
@@ -1,20 +1,24 @@
 
 function prepareFuelTransactionData (transactionList) {
-    let spentOnGas = 0;
+    let grossSpentOnGas = 0;
     let savedOnFuelPoints = 0;
     let fuelPointsRedeemed = 0;
+    let netSpentOnGas = 0;
 
     for (let i = 0; i < transactionList.length; i++) {
         const transaction = transactionList[i];
-        spentOnGas += transaction.price_per_gallon * transaction.gallons_filled;
+        grossSpentOnGas += transaction.price_per_gallon * transaction.gallons_filled;
         fuelPointsRedeemed += transaction.fuel_points_redeemed;
         savedOnFuelPoints += transaction.gallons_filled * (transaction.fuel_points_redeemed / 1000);
+        netSpentOnGas = grossSpentOnGas - savedOnFuelPoints;
+        
     }
 
     return {
-        spentOnGas: spentOnGas.toFixed(2),
+        grossSpentOnGas: grossSpentOnGas.toFixed(2),
         fuelPointsRedeemed,
         savedOnFuelPoints: savedOnFuelPoints.toFixed(2),
+        netSpentOnGas: netSpentOnGas.toFixed(2),
     }
     
 }
