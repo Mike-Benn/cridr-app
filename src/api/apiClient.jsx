54025daa2c71aa47f6ed0d80cae9930c766f30b4
Mike-Benn <mbenn49@wgu.edu>
2025-03-10 07:38:01 -0400

    Remove commented code

--- src/api/apiClient.jsx
+++ src/api/apiClient.jsx
@@ -1,6 +1,5 @@
 import axios from "axios"
 
-
 const apiClient = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
@@ -17,4 +16,47 @@ apiClient.interceptors.request.use(
     (error) => Promise.reject(error)
 );
 
+/*
+apiClient.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (err) => {
+        if (err.response?.status === 401 && !err.response.data?.refreshAttempted) {
+            try {
+                console.log("running");
+                return apiClient.post("/auth/refresh-token");
+            } catch (refreshError) {
+                Promise.reject(refreshError);
+            }
+        }
+        console.log("This is the error", err);
+        Promise.reject(err);
+    }
+)
+
+
+/*
+apiClient.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (accessError) => {
+        if (accessError.response && accessError.response.status === 401) {
+            try {
+                const refreshResponse = await apiClient.post("/auth/refresh-token");
+                if (refreshResponse.data?.data) {
+                    localStorage.setItem("accessToken", refreshResponse.data.data);
+
+                }
+
+            } catch (refreshError) {
+                Promise.reject(refreshError)
+            }
+
+        }
+        Promise.reject(accessError);
+    }
+)
+*/
 export default apiClient
