0a764e7679e9eb4b5ec808bbf4edc4910ba35182
Mike-Benn <mbenn49@wgu.edu>
2025-03-26 05:30:48 -0400

    Refactor token refreshing to req interceptor

--- src/api/apiClient.jsx
+++ src/api/apiClient.jsx
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { logoutAndRedirect, validateToken } from "../utils/auth/utils";
 
 const apiClient = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
@@ -6,57 +7,21 @@ const apiClient = axios.create({
 })
 
 apiClient.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("accessToken");
-        if (token) {
-            config.headers["Authorization"] = `Bearer ${token}`;
+    async (config) => {
+        if (config.url === "/auth/refresh-token" || config.url === "/auth/login") {
+            return config;
         }
-        return config;
+        try {
+            await validateToken(config);
+            return config;
+        } catch (error) {
+            if (error.response?.status === 401) {
+                logoutAndRedirect();
+            }
+            return Promise.reject(error);
+        }
     },
     (error) => Promise.reject(error)
-);
-
-/*
-apiClient.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async (err) => {
-        if (err.response?.status === 401 && !err.response.data?.refreshAttempted) {
-            try {
-                console.log("running");
-                return apiClient.post("/auth/refresh-token");
-            } catch (refreshError) {
-                Promise.reject(refreshError);
-            }
-        }
-        console.log("This is the error", err);
-        Promise.reject(err);
-    }
 )
 
-
-/*
-apiClient.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async (accessError) => {
-        if (accessError.response && accessError.response.status === 401) {
-            try {
-                const refreshResponse = await apiClient.post("/auth/refresh-token");
-                if (refreshResponse.data?.data) {
-                    localStorage.setItem("accessToken", refreshResponse.data.data);
-
-                }
-
-            } catch (refreshError) {
-                Promise.reject(refreshError)
-            }
-
-        }
-        Promise.reject(accessError);
-    }
-)
-*/
 export default apiClient
