5b95298a4aa7aaa51aaf0560d05cd8de8e2402a7
Mike-Benn <mbenn49@wgu.edu>
2025-03-29 05:15:38 -0400

    Life data fetching into dashboard from display

--- src/components/credit-card-offers/AvailableOffersDashboard.jsx
+++ src/components/credit-card-offers/AvailableOffersDashboard.jsx
@@ -1,19 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import GeneralButton from "../general/buttons/GeneralButton"
 import AvailableOffersDisplay from "./AvailableOffersDisplay";
+import apiClient from "../../api/apiClient";
 
-function AvailableOffersDashboard() {    
+function AvailableOffersDashboard() {  
+    const [isLoading, setIsLoading] = useState(true);  
+    const [offers, setOffers] = useState([]);
+
     useEffect(() => {
         document.title = "Coupons | Cridr";
-
+        const getOffersByUserId = async () => {
+            try {
+                const response = await apiClient.get("/credit-card-offers/available");
+                if (response.status === 200 && Array.isArray(response.data.data)) {
+                    setOffers([...response.data.data]);
+                    setIsLoading(false);
+                }
+            } catch (error) {
+                console.log(error.response?.data?.message);
+                setIsLoading(false);
+                
+            }
+        }
+        getOffersByUserId();
     }, []);
     
+    if (isLoading) return <p>Loading...</p>
 
     return (
     <section>
         <Link to="new"><GeneralButton buttonType="button" buttonText="Add New Offer" /></Link>
-        <AvailableOffersDisplay />
+        <AvailableOffersDisplay offers={offers} setOffers={setOffers} />
     </section>
     )
 }
--- src/components/credit-card-offers/AvailableOffersDisplay.jsx
+++ src/components/credit-card-offers/AvailableOffersDisplay.jsx
@@ -1,34 +1,11 @@
-import { useContext, useEffect, useState } from "react"
 import apiClient from "../../api/apiClient"
 import { OfferListItem } from "../../utils/offers/utils";
 import { filterOutAndReturnById } from "../../utils/general/utils";
-import AuthContext from "../../auth/AuthContext";
-import { Link } from "react-router-dom";
-import GeneralButton from "../general/buttons/GeneralButton";
+import PropTypes from "prop-types";
 
 
-function AvailableOffersDisplay() {
-    const { setIsAuthenticated } = useContext(AuthContext);
-    const [offers, setOffers] = useState([])
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        const getOffersByUserId = async () => {
-            try {
-                const response = await apiClient.get("/credit-card-offers/available");
-                if (response.status === 200 && Array.isArray(response.data.data)) {
-                    setOffers([...response.data.data]);
-                    setIsLoading(false);
-                }
-            } catch (error) {
-                console.log(error.response?.data?.message);
-                setIsLoading(false);
-                
-            }
-        }
-        getOffersByUserId();
-    }, [setIsAuthenticated]);
-
+function AvailableOffersDisplay({ offers, setOffers }) {
+    
     const handleDeleteOffer = async (offerId) => {
         const offerAndUpdatedList = filterOutAndReturnById(offerId, offers, "available_offer_id");
         const previousOffers = [...offers];
@@ -37,19 +14,17 @@ function AvailableOffersDisplay() {
             await apiClient.delete(`/credit-card-offers/available?available_offer_id=${offerAndUpdatedList.filteredItem.available_offer_id}`)
         } catch (error) {
             setOffers(previousOffers);
+            alert("There was an error deleting offer, please try again.")
             console.error("Unable to delete offer.", error);
         }
-
     }
-
-    if (isLoading) return <p>Loading...</p>
 
     const offerListItems = offers.map(offer => 
         <OfferListItem key={offer.available_offer_id} offer={offer} onDelete={handleDeleteOffer} />
     );
+
     return (
         <>
-            <Link to="new"><GeneralButton buttonType="button" buttonText="New Offer" /></Link>
             <h1>Available Offers</h1>
             <ul>
                 {offerListItems}
@@ -57,6 +32,11 @@ function AvailableOffersDisplay() {
         </>
 
     )
+}
+
+AvailableOffersDisplay.propTypes = {
+    offers: PropTypes.array,
+    setOffers: PropTypes.func,
 }
 
 export default AvailableOffersDisplay
