9edae38ec3a87949ca516479521830778a0ce63c
Mike-Benn <mbenn49@wgu.edu>
2025-03-29 04:53:46 -0400

    Lift data fetching to dashboard out of summary

--- src/components/fuel/components/FuelTransactionDashboard.jsx
+++ src/components/fuel/components/FuelTransactionDashboard.jsx
@@ -1,16 +1,19 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
 import GeneralButton from "../../general/buttons/GeneralButton"
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import apiClient from "../../../api/apiClient";
 import UserFuelTransactionSummary from "./UserFuelTransactionSummary";
-import AuthContext from "../../../auth/AuthContext";
+import { SelectField } from "../../general/form-fields/InputFields";
+import { prepareFuelTransactionData } from "../../../utils/fuel/utils";
 
 
 function FuelTransactionDashboard() {
-    const { setIsAuthenticated } = useContext(AuthContext);
+    const defaultYearOption = [<option key="all" value="all">All</option>];
     const [transactionYears, setTransactionYears] = useState([]);
+    const [selectedYearId, setSelectedYearId] = useState("all");
     const [isLoading, setIsLoading] = useState(true);
+    const [preparedTransactionData, setPreparedTransactionData] = useState({});
 
     const navigate = useNavigate();
 
@@ -28,16 +31,47 @@ function FuelTransactionDashboard() {
                 setIsLoading(false);
             }
         }
+        
         getUniqueYearsByUserId();
-    }, [navigate, setIsAuthenticated]);
+    }, [navigate]);
 
+    useEffect(() => {
+        setIsLoading(true);
+        const getFuelTransactionData = async () => {
+            try {
+                if (selectedYearId === "all") {
+                    const response = await apiClient.get("/fuel-transaction");
+                    if (response.status === 200 && Array.isArray(response.data.data)) {
+                        setPreparedTransactionData(prepareFuelTransactionData(response.data.data));
+                        setIsLoading(false);
+                    }
+                } else {
+                    const response = await apiClient.get(`/fuel-transaction/?year=${selectedYearId}`);
+                    if (response.status === 200 && Array.isArray(response.data.data)) {
+                        setPreparedTransactionData(prepareFuelTransactionData(response.data.data));
+                        setIsLoading(false);
+                    }
+                }
+            } catch (error) {
+                console.log(error.response?.data?.message);
+                
+            }
+        }
+        getFuelTransactionData();
+    }, [selectedYearId])
+
+    const handleYearIdChange = (e) => {
+        setSelectedYearId(e.target.value)
+    }
+    
     if (isLoading) return <div>Loading...</div>;
     
     return (
         <section>
             <Link to="new-vehicle"><GeneralButton buttonType="button" buttonText="Add Vehicle" /></Link>
             <Link to="new"><GeneralButton buttonType="button" buttonText="Add Fuel Transaction" /></Link>
-            <UserFuelTransactionSummary transactionYears={transactionYears} />
+            <SelectField fieldId="fuel-transaction-summary-year-select" labelText="Select Transaction Year" optionList={transactionYears} onChange={handleYearIdChange} value={selectedYearId} optionTextAccessor="unique_year" optionIdAccessor="unique_year" defaultOptions={defaultYearOption} />
+            <UserFuelTransactionSummary preparedTransactionData={preparedTransactionData} />
         </section>
     )
 
--- src/components/fuel/components/UserFuelTransactionSummary.jsx
+++ src/components/fuel/components/UserFuelTransactionSummary.jsx
@@ -1,54 +1,9 @@
-import { useEffect, useState, useContext } from "react"
-import { SelectField } from "../../general/form-fields/InputFields"
-import apiClient from "../../../api/apiClient"
-import { useNavigate } from "react-router-dom"
-import { prepareFuelTransactionData } from "../../../utils/fuel/utils"
 import PropTypes from "prop-types"
-import AuthContext from "../../../auth/AuthContext"
 
-function UserFuelTransactionSummary({ transactionYears }) {
-    const defaultYearOption = [<option key="all" value="all">All</option>];
-    const [selectedYear, setSelectedYear] = useState("all");
-    const [preparedTransactionData, setPreparedTransactionData] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
-    const navigate = useNavigate();
-    const { setIsAuthenticated } = useContext(AuthContext);
-
-    useEffect(() => {
-        setIsLoading(true);
-        const getTransactionDataByYear = async () => {
-            try {
-                if (selectedYear === "all") {
-                    const response = await apiClient.get("/fuel-transaction");
-                    if (response.status === 200 && Array.isArray(response.data.data)) {
-                        setPreparedTransactionData(prepareFuelTransactionData(response.data.data));
-                        setIsLoading(false);
-                    }
-                } else {
-                    const response = await apiClient.get(`/fuel-transaction/?year=${selectedYear}`);
-                    if (response.status === 200 && Array.isArray(response.data.data)) {
-                        setPreparedTransactionData(prepareFuelTransactionData(response.data.data));
-                        setIsLoading(false);
-                    }
-                }
-            } catch (error) {
-                console.log(error.response?.data?.message);
-                setIsLoading(false);
-            }
-        }
-        getTransactionDataByYear();
-
-    }, [transactionYears, selectedYear, navigate, setIsAuthenticated])
-
-    const handleYearIdChange = (e) => {
-        setSelectedYear(e.target.value)
-    }
-
-    if (isLoading) return <div>Loading...</div>;
+function UserFuelTransactionSummary({ preparedTransactionData }) {
 
     return (
         <>
-            <SelectField fieldId="fuel-transaction-summary-year-select" labelText="Select Transaction Year" optionList={transactionYears} onChange={handleYearIdChange} value={selectedYear} optionTextAccessor="unique_year" optionIdAccessor="unique_year" defaultOptions={defaultYearOption} />
             <ul>
                 <li>Gross Gas Expenditure: {preparedTransactionData.grossSpentOnGas}</li>
                 <li>Fuel Points Redeemed: {preparedTransactionData.fuelPointsRedeemed}</li>
@@ -60,7 +15,7 @@ function UserFuelTransactionSummary({ transactionYears }) {
 }
 
 UserFuelTransactionSummary.propTypes = {
-    transactionYears: PropTypes.arrayOf(PropTypes.string),
+    preparedTransactionData: PropTypes.object,
 }
 
 export default UserFuelTransactionSummary
