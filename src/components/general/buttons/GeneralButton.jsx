f5096610ddf65e6f185f31f78f1ba61a89242af4
Mike-Benn <mbenn49@wgu.edu>
2025-04-07 07:52:35 -0400

    Add name and value to general button

--- src/components/general/buttons/GeneralButton.jsx
+++ src/components/general/buttons/GeneralButton.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 
-function GeneralButton ({  buttonType , buttonText , onClick }) {
+function GeneralButton ({  buttonType , buttonText , onClick, value, name }) {
     return (
-        <button type={buttonType} onClick={onClick}>{buttonText}</button>
+        <button type={buttonType} onClick={onClick} value={value} name={name} >{buttonText}</button>
     )
 }
 
@@ -10,6 +10,12 @@ GeneralButton.propTypes = {
     buttonType: PropTypes.string,
     buttonText: PropTypes.string,
     onClick: PropTypes.func,
+    value: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.bool,
+    ]),
+    name: PropTypes.string,
 }
 
 export default GeneralButton
