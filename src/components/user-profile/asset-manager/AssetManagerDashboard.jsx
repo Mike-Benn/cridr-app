18f8deedc2cf26e76c8b55e40f1edee260545f54
Mike-Benn <mbenn49@wgu.edu>
2025-04-07 07:53:51 -0400

    Add asset manager and dashboards for each asset

--- src/components/user-profile/asset-manager/AssetManagerDashboard.jsx
+++ src/components/user-profile/asset-manager/AssetManagerDashboard.jsx
@@ -1,14 +1,35 @@
 import GeneralButton from "../../general/buttons/GeneralButton";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import CreditCardManagerDashboard from "./credit-cards/CreditCardManagerDashboard";
+import VehicleManagerDashboard from "./vehicles/VehicleManagerDashboard";
+import BusinessManagerDashboard from "./businesses/BusinessManagerDashboard";
 
 function AssetManagerDashboard() {
+    
+    const [assetTabs, setAssetTabs] = useState({
+        tabIndex: 0,
+    });
+
+    const handleAssetTabsChange = (e) => {
+        const { name, value } = e.target;
+        setAssetTabs((prev) => ({ ...prev, [name]: Number(value) }))
+    }
+    console.log(assetTabs.tabIndex);
     return (
-        <>
-            <Link to="credit-cards"><GeneralButton buttonType="button" buttonText="Manage Credit Cards" /></Link>
-            <Link to="businesses"><GeneralButton buttonType="button" buttonText="Manage Businesses" /></Link>
-            <Link to="vehicles"><GeneralButton buttonType="button" buttonText="Manage Vehicles" /></Link>
-        </>
+        <div>
+            <header>
+                <GeneralButton buttonType="button" buttonText="Credit Cards" onClick={handleAssetTabsChange} value={0} name="tabIndex" />
+                <GeneralButton buttonType="button" buttonText="Businesses" onClick={handleAssetTabsChange} value={1} name="tabIndex" />
+                <GeneralButton buttonType="button" buttonText="Vehicles" onClick={handleAssetTabsChange} value={2} name="tabIndex" />
+            </header>
+            <section>
+                {assetTabs.tabIndex === 0 && <CreditCardManagerDashboard />}
+                {assetTabs.tabIndex === 1 && <BusinessManagerDashboard />}
+                {assetTabs.tabIndex === 2 && <VehicleManagerDashboard />}
+            </section>
+        </div>
     )
+    
 }
 
 export default AssetManagerDashboard
--- src/components/user-profile/asset-manager/credit-cards/CreditCardManagerDashboard.jsx
+++ src/components/user-profile/asset-manager/credit-cards/CreditCardManagerDashboard.jsx
@@ -1,14 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import NewCreditCardForm from "./NewCreditCardForm";
+import GeneralButton from "../../../general/buttons/GeneralButton";
+import apiClient from "../../../../api/apiClient";
 
 function CreditCardManagerDashboard() {
 
-    useEffect(() => {
-        document.title = "My credit cards | Cridr";
+    const [creditCards, setCreditCards] = useState([]);
+    const [uiState, setUiState] = useState({
+        isAddingCard: false,
+        isLoading: true,
     })
+    const toggleAddCard = () => {
+        setUiState((prev) => ({ ...prev, isAddingCard: !prev.isAddingCard }))
+    }
+    
+    useEffect(() => {
+        const getCreditCards = async () => {
+            try {
+                const response = await apiClient.get("/credit-cards");
+                if (response.data?.data && Array.isArray(response.data.data)) {
+                    setCreditCards(response.data.data);
+                    setUiState((prev) => ({ ...prev, isLoading: false} ))
+                }
+            } catch (error) {
+                console.log(error.response?.data?.message);
+            }
+        }
+        getCreditCards();
+    }, [])
 
+    if (uiState.isLoading) return <p>Loading...</p>
+    const creditCardList = <ul>{creditCards.map(card =>  <li key={card.credit_card_id}>{card.credit_card_name}</li>)}</ul>
     return (
-        <NewCreditCardForm />
+        <>
+            {!uiState.isAddingCard && creditCardList}
+            {uiState.isAddingCard && <NewCreditCardForm />}
+            {uiState.isAddingCard && <GeneralButton buttonType="button" buttonText="Cancel" onClick={toggleAddCard} />}
+            {!uiState.isAddingCard && <GeneralButton buttonType="button" buttonText="Add New Card" onClick={toggleAddCard} />}
+        </>
     )
 }
 
