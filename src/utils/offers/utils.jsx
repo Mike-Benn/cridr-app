80e4da8bb736527aa3a1983dc430e856b7d3b95c
Mike-Benn <mbenn49@wgu.edu>
2025-06-15 05:41:07 -0400

    Fix typo in object key

--- src/utils/offers/utils.jsx
+++ src/utils/offers/utils.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 
 function OfferListItem({ offer, onDelete }) {
-    return <li>{offer.credit_card_name} - {offer.available_offer_participating_business} - {offer.cashback_rate}% - Expires: {new Date(offer.available_offer_expiration_date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })} <DeleteListItemButton id={offer.available_offer_id} onClick={onDelete} /> </li>
+    return <li>{offer.credit_card_name} - {offer.available_offer_participating_business} - {offer.cashback_rate}% - Expires: {new Date(offer.available_offer_expiration_date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })} <DeleteListItemButton id={offer.offers_id} onClick={onDelete} /> </li>
 
 }
 
