62891100763c95047fd526a2ed30cea89bef36a9
Mike-Benn <mbenn49@wgu.edu>
2025-06-22 07:40:55 -0400

    Create top nav,profile menu drop down for top nav

---
+++ src/components/header/ProfileMenu.jsx
@@ -0,0 +1,58 @@
+import Menu from "@mui/material/Menu"
+import MenuItem from "@mui/material/MenuItem"
+import AccountIcon from "@mui/icons-material/AccountCircle"
+import { IconButton } from "@mui/material";
+import { useState, useContext } from "react";
+import AuthContext from "../../auth/AuthContext";
+import { Link } from "react-router-dom";
+
+function ProfileMenu() {
+
+
+    const { logout } = useContext(AuthContext);
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const toggleMenu = (event) => {
+        anchorEl ? setAnchorEl(null) : setAnchorEl(event.currentTarget);
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    }
+
+    const handleUserLogout = async () => {
+        try {
+            await logout();
+        } catch (error) {
+            console.error(error , "There was an issue communicating with the server.");
+        }
+    }
+
+    const isOpen = Boolean(anchorEl);
+    return (
+        <>
+            <IconButton onClick={toggleMenu}>
+                <AccountIcon fontSize="large" />
+            </IconButton>
+            <Menu
+                anchorEl={anchorEl}
+                open={isOpen}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left"
+                }}
+                transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left"
+                }}
+            >
+                <MenuItem component={Link} to="/profile">Your profile</MenuItem>
+                <MenuItem onClick={handleUserLogout}>Sign out</MenuItem>
+            </Menu>
+        </>
+    )
+}
+
+export default ProfileMenu
+
--- src/components/header/TopNav.jsx
+++ src/components/header/TopNav.jsx
@@ -1,36 +1,12 @@
-import { useLocation } from "react-router-dom"
-import { getPathName } from "../../utils/general/utils"
-import GeneralButton from "../general/buttons/GeneralButton"
-import { useContext } from "react";
-import AuthContext from "../../auth/AuthContext";
-import { Link } from "react-router-dom";
-
-
+import ProfileMenu from "./ProfileMenu"
+import Typography from "@mui/material/Typography"
+import styles from "./TopNav.module.css"
 function TopNav() {
-    const { logout } = useContext(AuthContext);
-    let path = useLocation();
-    path = getPathName(path.pathname);
     
-    const handleUserLogOut = async () => {
-        try {
-            await logout();
-        } catch (error) {
-            console.error(error , "There was an issue communicating with the server.");
-        }
-    }
-    // Home, prof picture
     return (
-        <header>
-            <h2>{path}</h2>
-            <ul>
-                <Link to="/"><li>Home</li></Link>
-                <Link to="/offers"><li>Offers</li></Link>
-                <Link to="/incentives"><li>Incentives</li></Link>
-                <Link to="/expenses"><li>Expenses</li></Link>
-                <Link to="/savings-summary"><li>Savings Summary</li></Link>
-            </ul>
-            <GeneralButton buttonType="button" buttonText="Log Out" onClick={handleUserLogOut} />
-            <Link to="/profile"><GeneralButton buttonType="button" buttonText="Edit Profile" /></Link>
+        <header className={styles.header}>
+            <Typography variant="h4" component="h1">Cridr App</Typography>
+            <ProfileMenu />
         </header>
     )
 }
