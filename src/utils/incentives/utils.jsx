3a13e76c367dc9d150a7e68c1d25fc6e2ebacb4f
Mike-Benn <mbenn49@wgu.edu>
2025-06-29 07:26:58 -0400

    Remove unused utils

--- src/utils/incentives/utils.jsx
+++ src/utils/incentives/utils.jsx
@@ -1,29 +1,3 @@
-function prepareIncentiveTransactionData(transactionList) {
-    let preparedData = {};
-    for (let i = 0; i < transactionList.length; i++) {
-        let businessName = transactionList[i].business_name;
-        if (preparedData[businessName]) {
-            preparedData[businessName].incentiveAmount += transactionList[i].incentive_amount;
-        } else {
-            preparedData[businessName] = { 
-                businessId: transactionList[i].business_id, 
-                incentiveAmount: transactionList[i].incentive_amount
-            };
-        }
-    }
-    return preparedData;
-}
-
-function createIncentiveListItems(preparedData) {
-    if (preparedData) {
-        const listItems = Object.entries(preparedData).map(([businessName, transactionData]) => 
-            <li key={transactionData.businessId}>{businessName}: {transactionData.incentiveAmount}</li>
-        )
-        return listItems;
-    }
-    return;
-}
-
 function insertByDate(arr, incentive) {
     const newArray = [];
     let index = 0;
@@ -52,7 +26,5 @@ function insertByDate(arr, incentive) {
 
 
 export {
-    prepareIncentiveTransactionData,
-    createIncentiveListItems,
     insertByDate,
 }
