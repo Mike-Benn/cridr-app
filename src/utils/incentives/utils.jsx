640cddb53e3d7a87f56dc01945e7210560a9e3e3
Mike-Benn <mbenn49@wgu.edu>
2025-06-29 05:54:19 -0400

    Add insert into array by sorted date util

--- src/utils/incentives/utils.jsx
+++ src/utils/incentives/utils.jsx
@@ -22,10 +22,37 @@ function createIncentiveListItems(preparedData) {
         return listItems;
     }
     return;
-    
 }
+
+function insertByDate(arr, incentive) {
+    const newArray = [];
+    let index = 0;
+    let inserted = false;
+    while (newArray.length < 5 && arr[index]) {
+        const currIncentive = arr[index];
+        if (!inserted && incentive.transaction_date >= currIncentive.transaction_date) {
+            newArray.push(incentive);
+            inserted = true;
+            if (newArray.length < 5) {
+                newArray.push(currIncentive);
+            } else {
+                break;
+            }
+        } else {
+            newArray.push(currIncentive);
+        }
+        index++;
+    }
+    if (newArray.length < 5 && !inserted) {
+        newArray.push(incentive)
+    }
+
+    return { newArray, inserted }
+}
+
 
 export {
     prepareIncentiveTransactionData,
     createIncentiveListItems,
+    insertByDate,
 }
