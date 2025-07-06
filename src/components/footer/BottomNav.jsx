f74f7f1da3c95b1fdc2dbefbffa43d6c2f8ff6b1
Mike-Benn <mbenn49@wgu.edu>
2025-07-06 07:59:32 -0400

    Fix zindex of bottom nav

--- src/components/footer/BottomNav.jsx
+++ src/components/footer/BottomNav.jsx
@@ -34,6 +34,7 @@ function BottomNav() {
                     left: 0,
                     right: 0,
                     bottom: 0,
+                    zIndex: 1300,
                 }}
             >
                 <BottomNavigationAction label="Home" icon={<HomeIcon />} component={Link} to="/" value="/" />
