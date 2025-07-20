501a606d20625b99d67d73f34ee692f137bcf5d9
Mike-Benn <mbenn49@wgu.edu>
2025-07-20 03:00:58 -0400

    Add form for creating new offers

---
+++ src/components/offers/NewOfferForm.jsx
@@ -0,0 +1,162 @@
+import { useForm, Controller } from "react-hook-form";
+import Select from "@mui/material/Select"
+import MenuItem from "@mui/material/MenuItem"
+import styles from "./NewOfferForm.module.css"
+import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import apiClient from "../../api/apiClient";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel"
+
+function NewOfferForm({ uiState, handlers }) {
+    const { handleSubmit, control, watch, reset, resetField, formState: { isSubmitting } } = useForm({
+        defaultValues: {
+            creditCardId: "",
+            businessId: "",
+            description: "",
+            expirationDate: "",
+        }
+    });
+    const insertNewOfferIntoList = (offerList, newOffer) => {
+        if (!offerList) return [newOffer];
+        const newArray = [];
+        let inserted = false;
+        for (let i = 0; i < offerList.length; i++) {
+            const offer = offerList[i];
+            if (!inserted && newOffer.expiration_date < offer.expiration_date) {
+                newArray.push(newOffer);
+                inserted = true;
+            }
+            newArray.push(offer);
+        }
+        if (!inserted) newArray.push(newOffer);
+        return newArray;
+    }
+    const submitForm = async (data, e) => {
+        const submitAction = e.nativeEvent.submitter.value;
+        const newOffer = {
+            credit_card_id: data.creditCardId,
+            business_id: data.businessId,
+            offer_description: data.description,
+            expiration_date: data.expirationDate,
+        }
+        try {
+            const newOfferResponse = await apiClient.post("/offers", newOffer);
+            newOffer["offers_id"] = newOfferResponse.data.data[0].offers_id;
+            newOffer["business_name"] = uiState.businessMap[newOffer.business_id].business_name;
+            const updatedMapList = insertNewOfferIntoList(uiState.availableOffersMap[newOffer.credit_card_id], newOffer);
+            handlers.setUiState((prev) => ({
+                ...prev,
+                availableOffersMap: {
+                    ...prev.availableOffersMap,
+                    [newOffer.credit_card_id]: updatedMapList,
+                }
+            }))
+
+            // Maybe doesn't need reset call
+            if (submitAction === "submit") {
+                handlers.toggleViewMode()
+            } else {
+                reset();
+            }
+        } catch (error) {
+            console.log(error.response?.data?.message);
+            console.log(error)
+        }
+    }
+    
+    return (
+        <form className={styles.form} onSubmit={handleSubmit(submitForm)}>
+            <Typography variant="h6">Offer Details</Typography>
+            <div className={styles.formFields}>
+                <Controller
+                    name="creditCardId"
+                    control={control}
+                    rules={{ required: "You must select a credit card" }}
+                    render={({ field, fieldState: { error } }) => (
+                        <FormControl fullWidth error={!!error} >
+                            <InputLabel id="select-card-new-offer-label">Card Account</InputLabel>
+                            <Select
+                                {...field}
+                                labelId="select-card-new-offer-label"
+                                label="Card Account"
+                            >
+                                {uiState.creditCardList.map(card => (
+                                    <MenuItem key={card.credit_card_id} value={card.credit_card_id}>
+                                        {card.credit_card_name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    )}
+                />
+                <Controller
+                    name="businessId"
+                    control={control}
+                    rules={{ required: "You must select a business" }}
+                    render={({ field, fieldState: { error } }) => (
+                        <FormControl fullWidth error={!!error}>
+                            <InputLabel id="select-business-new-offer-label">Business</InputLabel>
+                            <Select
+                                {...field}
+                                labelId="select-business-new-offer-label"
+                                label="Business"
+                            >
+                                {uiState.businessList.map(biz => (
+                                    <MenuItem key={biz.business_id} value={biz.business_id}>
+                                        {biz.business_name}
+                                    </MenuItem>
+                                ))}   
+                            </Select>
+                        </FormControl>
+                    )}
+                />
+                <Controller
+                    name="description"
+                    control={control}
+                    render={({ field, fieldState: { error } }) => (
+                        <TextField
+                            {...field}
+                            label="Description"
+                            error={!!error}
+                            helperText={error ? error.message : null}
+                            fullWidth
+                            variant="outlined"  
+                        />
+                    )}
+                />
+                <Controller
+                    name="expirationDate"
+                    control={control}
+                    rules={{ required: "Expiration date is required" }}
+                    render={({ field, fieldState: { error } }) => (
+                        <TextField
+                            {...field}
+                            label="Transaction date"
+                            type="date"
+                            variant="outlined"
+                            slotProps={{
+                                inputLabel: {
+                                    shrink: true,
+                                }
+                            }}
+                            error={!!error}
+                            helperText={error ? error.message : null}
+                            fullWidth
+                        />
+                    )}
+                />
+            </div>
+            <div className={styles.buttonContainer}>
+                <Button type="submit" variant="contained" value="submit" disabled={isSubmitting}>Submit</Button>
+                <Button type="submit" variant="contained" value="submitAnother" disabled={isSubmitting}>Add another</Button>
+                <Button variant="contained" onClick={handlers.toggleViewMode}>Cancel</Button>
+            </div>
+
+        </form>
+    )
+
+}
+
+export default NewOfferForm
