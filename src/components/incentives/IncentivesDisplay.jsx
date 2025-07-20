1f99ae04a030acf713d479f2ab44f8d44daf988c
Mike-Benn <mbenn49@wgu.edu>
2025-07-20 05:44:19 -0400

    Overhaul incentives display layout

--- src/components/incentives/IncentivesDisplay.jsx
+++ src/components/incentives/IncentivesDisplay.jsx
@@ -2,14 +2,37 @@ import RecentTransactionsMobile from "./RecentTransactionsMobile"
 import Button from "@mui/material/Button"
 import styles from "./IncentivesDisplay.module.css"
 import Typography from "@mui/material/Typography"
-function IncentivesDisplay({ incentiveTransactionList, handlers }) {
+import { formatCurrency } from "../../utils/general/utils"
+function IncentivesDisplay({ uiState, handlers }) {
 
+    const calculateIncentiveTotal = () => {
+        const incentiveList = uiState.incentiveTransactionList;
+        let total = 0
+        for (let i = 0; i < incentiveList.length; i++) {
+            total += incentiveList[i].incentive_amount;
+        }
+        total = formatCurrency(total);
+        return total;
+    }
+
+    const incentiveTotal = calculateIncentiveTotal();
     return (
-        <div className={styles.displayContainer}>
-            <Button variant="contained" onClick={handlers.toggleViewMode} sx={{ alignSelf: "center" }}>Add Incentive</Button>
-            <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>Recent Incentives</Typography>
-            <RecentTransactionsMobile incentiveTransactionList={incentiveTransactionList} />
-        </div>
+        <main className={styles.main}>
+            <header className={styles.header}>
+                <Typography variant="h6" sx={{ alignSelf: "center", fontWeight: "bold" }}>Incentives</Typography>
+                <div className={styles.subheader}>
+                    <div className={styles.amountSaved}>
+                        <Typography variant="body2" sx={{ fontSize: "1.5rem" }}>{`${incentiveTotal}`}</Typography>
+                        <Typography variant="caption">Total amount earned</Typography>
+                    </div>
+                    <Button variant="contained" size="small" onClick={handlers.toggleViewMode} sx={{ alignSelf: "center" }}>Add incentive</Button>
+                </div>
+            </header>
+            <section className={styles.recentIncentives}>
+                <Typography variant="subtitle1">Recent Incentives</Typography>
+                <RecentTransactionsMobile uiState={uiState} />
+            </section>
+        </main>
         
     )
 }
