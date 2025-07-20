daaeffc7cea080c26413beab008a1fca29d868b0
Mike-Benn <mbenn49@wgu.edu>
2025-07-20 03:00:03 -0400

    Remove outdated offers components

--- src/components/credit-card-offers/OfferListItem.jsx
+++
@@ -1,7 +0,0 @@
-import OfferActions from "./OfferActions"
-
-function OfferListItem({ offer, handlers }) {
-    return <li>{offer.credit_card_name} | {offer.business_name} | {offer.offer_description} | Expires: {new Date(offer.expiration_date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })} <OfferActions offer={offer} handlers={handlers} /> </li>
-}
-
-export default OfferListItem
--- src/components/credit-card-offers/RedeemedOfferListItem.jsx
+++
@@ -1,5 +0,0 @@
-function RedeemedOfferListItem({ offer }) {
-    return <li>${offer.amount_saved} | {offer.credit_card_name} | {offer.business_name} | {offer.offer_description} | Redemption Date: {new Date(offer.redeemed_date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })}</li>
-}
-
-export default RedeemedOfferListItem
