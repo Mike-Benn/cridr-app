162db7a5a5a44eb34a8cdfa83aa5ea3771169c7f
Mike-Benn <mbenn49@wgu.edu>
2025-07-28 07:33:11 -0400

    Abstract offers list display into own component

---
+++ src/components/offers/ActiveOffersList.jsx
@@ -0,0 +1,37 @@
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
+import ListItemText from "@mui/material/ListItemText"
+import { readableDate } from "../../utils/offers/utils";
+import DeleteIcon from "@mui/icons-material/Clear"
+import IconButton from "@mui/material/IconButton"
+
+
+/*
+<ListItem divider key={offer.offers_id}>
+                                <ListItemText primary={`${offer.business_name} | ${offer.offer_description}`} secondary={`Expiration: ${readableDate(offer.expiration_date)}`} />
+                                <IconButton onClick={() => handleDeleteOffer(offer.offers_id)}>
+                                    <DeleteIcon />
+                                </IconButton>
+                            </ListItem>
+*/
+
+function ActiveOffersList({ uiState }) {
+    const offersList = uiState.availableOffersList;
+    return (
+        <>
+            {offersList.length > 0 && 
+                <div>
+                    <List>
+                        {offersList.map(offer =>
+                            <ListItem divider key={offer.offers_id}>
+                                <ListItemText primary={`${offer.business_name} | ${offer.offer_description}`} secondary={`Expiration: ${readableDate(offer.expiration_date)}`} />
+                            </ListItem>
+                        )}
+                    </List>
+                </div>
+            }
+        </>
+    )
+}
+
+export default ActiveOffersList
