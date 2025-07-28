6d9e4f1fb2e62b7576dadfaddc0ec30201d88b74
Mike-Benn <mbenn49@wgu.edu>
2025-07-28 04:13:04 -0400

    Remove list trimming for incentive display

--- src/components/incentives/RecentTransactionsMobile.jsx
+++ src/components/incentives/RecentTransactionsMobile.jsx
@@ -2,16 +2,19 @@ import List from "@mui/material/List"
 import ListItem from "@mui/material/ListItem"
 import ListItemText from "@mui/material/ListItemText"
 import { readableDate } from "../../utils/offers/utils"
+import Typography from "@mui/material/Typography"
+import { formatCurrency } from "../../utils/general/utils"
 function RecentTransactionsMobile({ uiState }) { 
-    const listHead = uiState.incentiveTransactionList.slice(0, 5);
+    const expenseList = uiState.incentiveTransactionList;
     return (
         <>
-            {listHead.length > 0 && 
+            {expenseList.length > 0 && 
                 <div>
                     <List>
-                        {listHead.map(trx => 
+                        {expenseList.map(trx => 
                             <ListItem divider key={trx.transaction_incentive_id}>
                                 <ListItemText primary={`${uiState.businessMap[trx.business_id].business_name} | ${trx.incentive_description}`} secondary={`Date: ${readableDate(trx.transaction_date)}`} />
+                                <Typography variant="subtitle1">{formatCurrency(trx.incentive_amount)}</Typography>
                             </ListItem>
                         )}
                     </List>
