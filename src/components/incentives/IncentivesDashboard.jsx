4b47993e362316fe639d0d0afb9946e8aafbf175
Mike-Benn <mbenn49@wgu.edu>
2025-07-28 04:14:33 -0400

    Add uiFlags for managing loading state Add data fetching for incentive list display

--- src/components/incentives/IncentivesDashboard.jsx
+++ src/components/incentives/IncentivesDashboard.jsx
@@ -1,57 +1,114 @@
 import { useEffect, useState } from "react"
 import apiClient from "../../api/apiClient";
 import NewIncentiveForm from "./NewIncentiveForm";
-import IncentivesDisplay from "./IncentivesDisplay"
+import IncentivesDisplay from "./IncentivesDisplay";
+
 
 function IncentivesDashboard() {
 
     const [uiState, setUiState] = useState({
-        viewMode: "loading",
+        viewMode: "viewing",
         businessList: [],
         businessMap: {},
         incentiveTransactionList: [],
+        incentiveSum: 1,
+        uniqueMonthList: [],
+        selectedMonth: "",
+    })
+    const [uiFlags, setUiFlags] = useState({
+        loadingCount: 1,
+        needsRefreshed: false,
     })
 
+    const startLoad = () => setUiFlags(prev => ({
+        ...prev,
+        loadingCount: prev.loadingCount + 1
+    }))
+
+    const endLoad = () => setUiFlags(prev => ({
+        ...prev,
+        loadingCount: prev.loadingCount - 1
+    }))
+
+    const getIncentives = async () => {
+        startLoad();
+        try {
+            const [incentivesResponse, statsResponse] = await Promise.all([
+                apiClient.get("/incentives", { params: { order: "DESC", month: uiState.selectedMonth }}),
+                apiClient.get("/incentives/stats", { params: { month: uiState.selectedMonth }})
+            ])
+            setUiState(prev => ({
+                ...prev,
+                incentiveTransactionList: incentivesResponse.data.data,
+                incentiveSum: statsResponse.data.data[0].total,
+            }))
+        } catch (error) {
+            console.log(error)
+        }
+        endLoad();
+    }
 
     useEffect(() => {
         document.title = "Incentives | Cridr"
         const getData = async () => {
             try {
-                const [businessesResponse, incentivesResponse] = await Promise.all([
+                const [businessesResponse, uniqueMonthsResponse] = await Promise.all([
                     apiClient.get("/businesses", { params: { featureNames: "Incentives", includeMap: "true" }}),
-                    apiClient.get("/incentives", { params: { order: "DESC" }})
+                    apiClient.get("/incentives/months")
                 ])
+                const firstMonth = uniqueMonthsResponse.data.data[0].month ?? "";
                 setUiState((prev) => ({
                     ...prev,
                     businessList: businessesResponse.data.data.businessList,
                     businessMap: businessesResponse.data.data.businessMap,
-                    incentiveTransactionList: incentivesResponse.data.data,
-                    viewMode: "viewing",
-
+                    uniqueMonthList: uniqueMonthsResponse.data.data,
+                    selectedMonth: firstMonth,
                 }))
             } catch (error) {
                 console.log(error.response?.data?.message)
             }
+            endLoad();
+
         }
         getData();
     }, [])
+
+    useEffect(() => {
+        if (uiState.selectedMonth === "") return;
+        getIncentives();
+    }, [uiState.selectedMonth])
+
+    useEffect(() => {
+        if (!uiFlags.needsRefreshed || uiState.viewMode !== "viewing") return;
+        getIncentives();
+
+        setUiFlags(prev => ({ ...prev, needsRefreshed: false }))
+    }, [uiFlags.needsRefreshed, uiState.viewMode])
 
     const toggleViewMode = () => {
         setUiState((prev) => ({ ...prev, viewMode: prev.viewMode === "editing" ? "viewing" : "editing" }));
     }
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUiState(prev => ({
+            ...prev,
+            [name]: value,
+        }))
+    }
+    
     const handlers = {
         toggleViewMode,
         setUiState,
+        handleChange,
+        setUiFlags,
     }
+    const isLoading = uiFlags.loadingCount > 0;
 
-    const stateData = {
-        incentiveTransactionList: uiState.incentiveTransactionList,
-    }
     return (
         <>
-            {uiState.viewMode === "viewing" && <IncentivesDisplay uiState={uiState} handlers={handlers} />}
-            {uiState.viewMode === "editing" && <NewIncentiveForm businessList={uiState.businessList} handlers={handlers} stateData={stateData}/> }
+            {!isLoading && uiState.viewMode === "viewing" && <IncentivesDisplay uiState={uiState} handlers={handlers} />}
+            {!isLoading && uiState.viewMode === "editing" && <NewIncentiveForm uiState={uiState} handlers={handlers} /> }
         </>
     )
 }
