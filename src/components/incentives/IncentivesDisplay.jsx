1a720efda739c18cd58b6e8fc4621eb78ede2784
Mike-Benn <mbenn49@wgu.edu>
2025-07-28 04:20:52 -0400

    Add select component to filter incentive list

--- src/components/incentives/IncentivesDisplay.jsx
+++ src/components/incentives/IncentivesDisplay.jsx
@@ -2,38 +2,48 @@ import RecentTransactionsMobile from "./RecentTransactionsMobile"
 import Button from "@mui/material/Button"
 import styles from "./IncentivesDisplay.module.css"
 import Typography from "@mui/material/Typography"
-import { formatCurrency } from "../../utils/general/utils"
+import FormControl from "@mui/material/FormControl"
+import InputLabel from "@mui/material/InputLabel"
+import Select from "@mui/material/Select"
+import MenuItem from "@mui/material/MenuItem"
+import { formatCurrency, readableMonth } from "../../utils/general/utils"
+
 function IncentivesDisplay({ uiState, handlers }) {
 
-    const calculateIncentiveTotal = () => {
-        const incentiveList = uiState.incentiveTransactionList;
-        let total = 0
-        for (let i = 0; i < incentiveList.length; i++) {
-            total += incentiveList[i].incentive_amount;
-        }
-        total = formatCurrency(total);
-        return total;
-    }
-
-    const incentiveTotal = calculateIncentiveTotal();
     return (
         <main className={styles.main}>
             <header className={styles.header}>
                 <Typography variant="h6" sx={{ alignSelf: "center", fontWeight: "bold" }}>Incentives</Typography>
                 <div className={styles.subheader}>
-                    <div className={styles.amountSaved}>
-                        <Typography variant="body2" sx={{ fontSize: "1.5rem" }}>{`${incentiveTotal}`}</Typography>
+                    <div className={styles.monthlySave}>
+                        <Typography variant="body2" sx={{ fontSize: "1.5rem" }}>{formatCurrency(uiState.incentiveSum)}</Typography>
                         <Typography variant="caption">Total amount earned</Typography>
                     </div>
                     <Button variant="contained" size="small" onClick={handlers.toggleViewMode} sx={{ alignSelf: "center" }}>Add incentive</Button>
                 </div>
             </header>
-            <section className={styles.recentIncentives}>
-                <Typography variant="subtitle1" sx={{ paddingLeft: "16px" }}>Recent Incentives</Typography>
-                <RecentTransactionsMobile uiState={uiState} />
+            {uiState.incentiveTransactionList.length > 0 && <section className={styles.recentIncentives}>
+                <FormControl fullWidth>
+                    <InputLabel id="select-incentive-month-label">Month</InputLabel>
+                    <Select
+                        labelId="select-incentive-month-label"
+                        value={uiState.selectedMonth}
+                        label="Month"
+                        onChange={handlers.handleChange}
+                        name="selectedMonth"
+                    >
+                        {uiState.uniqueMonthList.map(month =>
+                            <MenuItem key={month.month} value={month.month}>{readableMonth(month.month)}</MenuItem>
+                        )}
+                    </Select>
+                </FormControl>
+                <div className={styles.incentivesList}>
+                    <Typography variant="subtitle1" sx={{ paddingLeft: "16px" }}>Recent Incentives</Typography>
+                    <RecentTransactionsMobile uiState={uiState} />
+                </div>
             </section>
+            }
         </main>
-        
     )
 }
 
