ea66ae9f6e64c544bee9c85d84e4f3878423190a
Mike-Benn <mbenn49@wgu.edu>
2025-08-04 01:29:46 -0400

    Create tab panel for Tabs component

---
+++ src/components/home/TabPanel.jsx
@@ -0,0 +1,14 @@
+export default function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            className={other.className}
+        >
+            {value === index && children}
+        </div>
+    )
+}
